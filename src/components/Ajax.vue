704faabd3bc244f9097526238b7b14bc6bf9e19b
Brenolima03 <brenocslima@outlook.com>
2024-03-18 10:45:30 -0400

    creating project

---
+++ src/components/Ajax.vue
@@ -0,0 +1,31 @@
+<template>
+	<div>
+		<h1>Todos</h1>
+		<ul>
+			<li v-for="todo in todos" :key="todo.id" style="white-space: nowrap;">
+				{{ todo.id }} - {{ todo.title }} - {{ todo.completed }}
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+	import axios from 'axios';
+
+	export default {
+		data() {
+			return {
+				todos: []
+			};
+		},
+		mounted() {
+			axios.get('https://jsonplaceholder.typicode.com/todos/')
+			.then(response => {
+				this.todos = response.data;
+			})
+			.catch(error => {
+				console.error('Error when fething data: ', error);
+			});
+		}
+	};
+</script>
